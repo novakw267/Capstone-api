819a5c81e641ab4668e3b5a0397281131f4c9ced
novakw267 <novakw267@gmail.com>
2017-03-27 15:44:13 -0400

    Added the user ID to blog
    I ran a reference migration to add UserID to blogs
    This also added the foreign key between the user and blogs
    As well adding a migration between the two of them
    and the user ID in the blog schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20170327194124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "Title"
+    t.string   "Subject"
+    t.string   "Body"
+    t.string   "Author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id", using: :btree
+  end
 
   create_table "examples", force: :cascade do |t|
     t.text     "text",       null: false
@@ -33,5 +44,6 @@ ActiveRecord::Schema.define(version: 2) do
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
+  add_foreign_key "blogs", "users"
   add_foreign_key "examples", "users"
 end
