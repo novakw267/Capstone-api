16654909e4a1b7365fe87a36e59636310b28ee25
novakw267 <novakw267@gmail.com>
2017-03-28 11:33:39 -0400

    Added the statments for the relationships
    In each model I added the belongs to and has many

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Blog < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true
   validates :subject, presence: true
   validates :body, presence: true
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Comment < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
+  belongs_to :blogs
   validates :author, presence: true
   validates :idea, presence: true
 end
