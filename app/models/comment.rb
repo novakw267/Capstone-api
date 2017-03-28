6d585ea88768fa15baf5a21cd42b2034025b3715
novakw267 <novakw267@gmail.com>
2017-03-28 11:20:08 -0400

    Comment curl scripts
    I got all the curl scripts for comment working prior to adding the
    relationships. I also removed the show.sh scripts because I wouldn't
    need them since I only want to be able to display the most recent blogs.
    I may revisit an additional script if I have time to add a search
    function.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class Comment < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   validates :author, presence: true
   validates :idea, presence: true
 end
---
+++ scripts/comments/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/comments/${ID}"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "comment": {
+      "author": "'"${AUTHOR}"'",
+      "idea": "'"${IDEA}"'"
+    }
+  }'
+
+echo
